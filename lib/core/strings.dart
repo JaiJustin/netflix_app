dd550479f75bbeea3612f06d5d48b7517e2ab3a5
JaiJustin <jaijustin4@gmail.com>
2022-08-24 15:36:10 +0530

    feat:ScreenHome API integrated
    Changes
    
    * Fetch trending movie images from the server by Api integration
    * display that image in ScreensDownloads
    
    Packages
    
     dependencies
    
      * flutter_bloc: ^8.1.1
      * json_serializable: ^6.3.1
      * json_annotation: ^4.6.0
      * dartz: ^0.10.1
      * dio: ^4.0.6
      * get_it: ^7.2.0
      * injectable: ^1.5.3
    
     dev_dependencies
    
      * flutter_lints: ^2.0.0
      * build_runner: ^2.2.0
      * freezed: ^2.1.0+1
      * injectable_generator: ^1.5.4

---
+++ lib/core/strings.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/widgets.dart';
+
+const kBaseUrl = 'https://api.themoviedb.org/3';
+
+const imageAppendUrl = 'https://image.tmdb.org/t/p/w500';
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:netflix_app/application/downloads/downloads_bloc.dart';
 import 'package:netflix_app/core/colors/colors.dart';
+import 'package:netflix_app/domain/core/di/injectable.dart';
 import 'package:netflix_app/presentation/main_page/screen_main_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await configureInjection();
   runApp(const MyApp());
 }
 
@@ -11,19 +16,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(backgroundColor: Colors.transparent),
-        scaffoldBackgroundColor: kBackgroundColor,
-        primarySwatch: Colors.blue,
-        backgroundColor: Colors.black,
-        textTheme: const TextTheme(
-          bodyText1: TextStyle(color: Colors.white),
-          bodyText2: TextStyle(color: Colors.white),
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (ctx) => getIt<DownloadsBloc>())],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          appBarTheme: const AppBarTheme(backgroundColor: Colors.transparent),
+          scaffoldBackgroundColor: kBackgroundColor,
+          primarySwatch: Colors.blue,
+          backgroundColor: Colors.black,
+          textTheme: const TextTheme(
+            bodyText1: TextStyle(color: Colors.white),
+            bodyText2: TextStyle(color: Colors.white),
+          ),
         ),
+        home: ScreenManPage(),
       ),
-      home: ScreenManPage(),
     );
   }
 }
